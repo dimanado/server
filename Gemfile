6138ed72ec07785e8d17fb8545a01fff8733e635
Kostya <konstantin.mikhaskov@gmail.com>
2015-06-23 18:08:53 +0300

    final server version

--- Gemfile
+++ Gemfile
@@ -8,7 +8,8 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
-gem 'new_mach_gem', :git => 'https://github.com/dimanado/new_mach_gem.git'
+gem 'new_math', '1.0.1', :git => 'https://github.com/FeniX717/new_math.git'
+gem 'new_mach_gem','1.0.1', :git => 'https://github.com/dimanado/new_mach_gem.git'
 
 group :development, :test do
   gem 'byebug'
--- app/controllers/new_mach_controller.rb
+++ app/controllers/new_mach_controller.rb
@@ -2,6 +2,11 @@ class NewMachController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def index
-    @puts=NewMachGem::MathematicalOperations.operations(params[:x1].to_i,params[:x2].to_i,params[:str])
+  	if (params[:str]=='sin' || params[:str]=='cos')
+  	  @puts=NewMath::MathPackage.sin_and_cos(params[:x1].to_i, params[:str])
+  	else
+  	  @puts=NewMath::MathPackage.operations(params[:x1].to_i,params[:x2].to_i,params[:str])
+  	end
+    render json: @puts, status: :ok
   end
 end
